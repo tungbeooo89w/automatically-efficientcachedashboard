45571da35b730dbfc88478b72d580b34d6d47bbc
tungbeooo89w <cpt.turtlepun0suy@hotmail.com>
2024-05-08 01:47:00 +0700

    update project

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+function countBits(num) {
+  const result = new Array(num + 1).fill(0);
+  for (let i = 1; i <= num; i++) {
+    result[i] = result[i >> 1] + (i & 1);
+  }
+  return result;
+}
