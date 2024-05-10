c42e199977970d5bf5d341faa2c378c0608451c8
tungbeooo89w <cpt.turtlepun0suy@hotmail.com>
2024-05-10 08:25:51 +0700

    demo

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-function isValidBST(root) {
-  let prev = null;
-  return inorder(root);
-  function inorder(node) {
-    if (!node) return true;
-    if (!inorder(node.left)) return false;
-    if (prev !== null && node.val <= prev) return false;
-    prev = node.val;
-    return inorder(node.right);
-  }
-}
+const isEven = (num) => num % 2 === 0;
