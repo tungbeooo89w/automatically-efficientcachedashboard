67caba5bbd6d3ba76377e59fcc70a3b7758a31c0
tungbeooo89w <cpt.turtlepun0suy@hotmail.com>
2024-05-15 00:33:38 +0700

    demo

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function lengthOfLIS(nums) {
-  if (nums.length === 0) return 0;
-  const dp = new Array(nums.length).fill(1);
-  let max = 1;
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-        max = Math.max(max, dp[i]);
-      }
-    }
+function lengthOfLongestSubstring(s) {
+  if (s.length === 0) return 0;
+  const map = new Map();
+  let maxLength = 0;
+  let left = 0;
+  for (let right = 0; right < s.length; right++) {
+    if (map.has(s[right])) left = Math.max(left, map.get(s[right]) + 1);
+    map.set(s[right], right);
+    maxLength = Math.max(maxLength, right - left + 1);
   }
-  return max;
+  return maxLength;
 }
