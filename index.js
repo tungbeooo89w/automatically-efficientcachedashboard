2d07539ffb6d5978fa5c3e4cbbea11027faefdb6
tungbeooo89w <cpt.turtlepun0suy@hotmail.com>
2024-05-15 18:10:21 +0700

    feat

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-function lengthOfLongestSubstring(s) {
-  if (s.length === 0) return 0;
-  const map = new Map();
-  let maxLength = 0;
-  let left = 0;
-  for (let right = 0; right < s.length; right++) {
-    if (map.has(s[right])) left = Math.max(left, map.get(s[right]) + 1);
-    map.set(s[right], right);
-    maxLength = Math.max(maxLength, right - left + 1);
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  if (n % 2 === 0 || n % 3 === 0) return false;
+  let i = 5;
+  while (i * i <= n) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+    i += 6;
   }
-  return maxLength;
+  return true;
 }
